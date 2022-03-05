0138c53206ccbbdfb761944016ddd6118f026016
Mohamad Khawam <55813440+dominusmars@users.noreply.github.com>
2022-03-05 14:05:04 -0500

    Added testing and answer info

--- src/containers/CreateTest.js
+++ src/containers/CreateTest.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from "react"
 
-function CreateTest() {
+function CreateTest({ createTest }) {
     const [Test, setTest] = useState([])
 
     const deleteQuestion = (index) => {
@@ -12,14 +12,17 @@ function CreateTest() {
     const addQuestion = (info) => {
         var nTests = [...Test];
         nTests.push({
+            oquestion: info.qO,
             question: info.q,
             formula: info.f,
+            varibles: info.v,
             anwser: info.a
         })
         console.log(nTests)
         setTest(nTests)
     }
     const submitTest = () => {
+        console.log(Test)
 
     }
     return (
@@ -30,37 +33,53 @@ function CreateTest() {
                 var formula = document.getElementById("formulaInput").value
                 var anwser = document.getElementById("answerInput").value
 
+                var regex = /(\w+(?==))/gm
+                var varibles = question.match(regex)
+
+                var oQuestion = question
+
+                var matchRegex = /\w+=/gm
+                var newQuestion = question.replace(matchRegex, "")
+
+                console.log(question)
                 addQuestion({
-                    q: question,
+                    qO: oQuestion,
+                    q: newQuestion,
+                    v: varibles,
                     f: formula,
                     a: anwser
                 })
+                document.getElementById('questionInput').value = '';
+                document.getElementById("formulaInput").value = '';
+                document.getElementById("answerInput").value = ''
 
             }}>
                 <label htmlFor="question">Question: </label>
-                <input type="text" className="question" id="questionInput" />
+                <input type="text" className="question" id="questionInput" required />
                 <label htmlFor="answer">Formula: </label>
-                <input type="text" className="answer" id="formulaInput" />
+                <input type="text" className="answer" id="formulaInput" required />
                 <label htmlFor="answer">Answer: </label>
-                <input type="text" className="answer" id="answerInput" />
+                <input type="text" className="answer" id="answerInput" required />
                 <input type="submit" />
             </form>
-            {Test.map((question, index) => {
-                return (
-                    <div key={index}>
-                        <h1>{question.question}</h1>
-                        <p>{question.anwser}</p>
-                        <button onClick={(e) => {
-                            deleteQuestion(index)
-                        }}>Delete</button>
-                    </div>
-                )
-            })}
+            {
+                Test.map((question, index) => {
+                    return (
+                        <div key={index}>
+                            <h6>{question.question}</h6>
+                            <p>{question.anwser}</p>
+                            <button onClick={(e) => {
+                                deleteQuestion(index)
+                            }}>Delete</button>
+                        </div>
+                    )
+                })
+            }
             <button onClick={() => {
+                submitTest()
+            }}>Submit Test</button>
 
-            }}></button>
-
-        </div>
+        </div >
     )
 }
 
